008a8605b47ea3fc00746e43fef46315845eea9c
Vygintas Ranonis <vygintas.ranonis@gmail.com>
2019-12-01 16:55:00 +0200

    shortest path code

--- src/lib.spec.ts
+++ src/lib.spec.ts
@@ -1,9 +1,21 @@
 import { expect } from 'chai';
-import { getSum } from './lib';
+import { getShortestPath } from './lib';
 
-describe('getSum', () => {
+describe('shortestPath', () => {
+  const network = {
+    'Min'     : ['William', 'Jayden', 'Omar'],
+    'William' : ['Min', 'Noam'],
+    'Jayden'  : ['Min', 'Amelia', 'Ren', 'Noam'],
+    'Ren'     : ['Jayden', 'Omar'],
+    'Amelia'  : ['Jayden', 'Adam', 'Miguel'],
+    'Adam'    : ['Amelia', 'Miguel', 'Sofia', 'Lucas'],
+    'Miguel'  : ['Amelia', 'Adam', 'Liam', 'Nathan'],
+    'Noam'    : ['Nathan', 'Jayden', 'William'],
+    'Omar'    : ['Ren', 'Min', 'Scott']
+  };
 
-  it('should add two numbers', () => {
-    expect(getSum(2, 3)).to.equal(5);
+  it('should find the shortest path', () => {
+    expect(getShortestPath('Jayden', 'Adam', network)).to.deep.equal([ 'Jayden', 'Amelia', 'Adam' ]);
   });
+
 });
--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,29 @@
-export function getSum(n: number, m: number): number {
-  return n + m;
+function _traverse(path: string[], target: string, nextBatch: string[], network: {[key: string]: string[]}): string[] {
+  if (!nextBatch || !nextBatch.length) {
+    return null;
+  };
+  if (nextBatch.includes(target)) {
+    return [...path, target];
+  };
+  return nextBatch
+    .filter(n => !path.includes(n)) // avoiding circular paths
+    .reduce((shortestPath: string[], n) => {
+    const nextPath = _traverse([...path, n], target, network[n], network);
+    if (!nextPath) {
+      return shortestPath;
+    }
+    if (!shortestPath) {
+      return nextPath;
+    }
+    if (nextPath.length < shortestPath.length) {
+      return nextPath;
+    }
+    return shortestPath;
+    }, null);
+}
+
+// solution to https://www.interviewcake.com/question/python/mesh-message
+
+export function getShortestPath(origin: string, target: string, network: {[key: string]: string[]}): string[] {
+  return _traverse([origin], target, network[origin], network);
 }
